b84f6fac42342ff78b380250def25ba179af6e8b
Vincent-Nguyen-s <66347259+Vincent-Nguyen-s@users.noreply.github.com>
2020-06-17 17:07:47 +1000

    Update exercise3.py

--- week2/exercise3.py
+++ week2/exercise3.py
@@ -3,11 +3,12 @@
 
 
 def is_odd(a_number):
+                      
     """Return True if a_number is odd, and False if a_number is even.
 
     Look into modulo division using the '%' operator as one way of doing this.
     """
-    return None
+    return (a_number % 2) != 0 
 
 
 def fix_it(moves=True, should_move=True):
@@ -25,7 +26,14 @@ def fix_it(moves=True, should_move=True):
     Most people write this function with 4 return statements. 
     As an extra challenge, see if you can get that down to three.
     """
-    return None
+    if moves and should_move:
+        return "No Problem"
+    elif moves and not should_move:
+        return "Duct Tape"
+    elif not moves and not should_move:
+        return "No Problem"
+    elif not moves and should_move:
+        return "WD-40"
 
 
 def loops_1a():
@@ -35,7 +43,11 @@ def loops_1a():
     return a list of 10 items, each one a string with exacly one star in it.
     E.g.: ['*', '*', '*', '*', '*', '*', '*', '*', '*', '*']
     """
-    return None
+    star_list = []
+    for i in range(10):
+        star_list.append("*")
+
+    return star_list
 
 
 def loops_1c(number_of_items=5, symbol="#"):
@@ -45,7 +57,11 @@ def loops_1c(number_of_items=5, symbol="#"):
     string with exacly one symbol in it.
     E.g.: ['#', '#', '#', '#', '#']
     """
-    return None
+    symbol_list = []
+    for i in range(number_of_items):
+        symbol_list.append(symbol)
+
+    return symbol_list
 
 
 def loops_2():
@@ -66,7 +82,14 @@ def loops_2():
             ['*', '*', '*', '*', '*', '*', '*', '*', '*', '*'],
           ]
     """
-    return None
+    star_array = [] 
+    for j in  range(10):
+        star_list = []
+        for i in range(10):
+            star_list.append("*")
+        star_array.append(star_list)
+
+    return star_array
 
 
 def loops_3():
@@ -90,7 +113,14 @@ def loops_3():
     TIP: notice that this needs to to return strings of numbers,
          so call str(number) to cast.
     """
-    return None
+    number_array =[]
+    for i in range(10):
+        number_row = []
+        for j in range(10):
+            number_row.append(str(i))
+        number_array.append(number_row)
+    
+    return number_array
 
 
 def loops_4():
@@ -110,7 +140,16 @@ def loops_4():
       ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
     ]
     """
-    return None
+
+    number_array = []
+    for a in range(10):
+        number_line = []
+        for b in range(10):
+            number_line.append(str(b))
+        number_array.append(number_line)
+    print(number_array)
+
+    return(number_array)
 
 
 def loops_5():
@@ -137,7 +176,15 @@ def loops_5():
         "There are {} green bottles".format(8)
     you'll come to see the pros and cons of each over time.
     """
-    return None
+    number_grid = []
+    for i in range(10):
+        coordinates_row = []
+        for j in range(5):
+            coordinates_row.append('(i{}, j{})'.format(i,j))
+        number_grid.append(coordinates_row)
+    print(number_grid)
+
+    return number_grid
 
 
 def loops_6():
@@ -160,7 +207,13 @@ def loops_6():
     You can use a variable.
     TIP: look out for the starting condition.
     """
-    return None
+    wedge = []
+    for i in range(10):
+        row = []
+        for j in range(i+1):
+            row.append(str(j))
+        wedge.append(row)
+    return wedge
 
 
 def loops_7():
@@ -184,7 +237,21 @@ def loops_7():
     This is a hard problem. Use lots of experimentation and draw
     lots of diagrams!
     """
-    return None
+
+    pyramid = []
+    for i in range(0, 5):
+        row = []
+        for j in range(0, 5-i-1):
+            row.append(" ")
+        for j in range(0,2*i+1):
+            row.append("*")
+        for j in range(0, 5-i-1):
+            row.append(" ")
+        pyramid.append(row)
+    
+    print(pyramid)
+    return pyramid
+
 
 
 def lp(some_kind_of_list, exercise_name):
